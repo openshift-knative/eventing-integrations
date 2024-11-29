bbbfeab45f36550d6603255b8af91693e7d30282
Christoph Deppisch <cdeppisch@redhat.com>
2024-11-29 12:11:44 +0100

    Add AWS SNS Sink tests (#41)
    - Add unit tests that uses the AWS SNS sink, verify that data gets published with an AWS SQS notification endpoint subscriber
    - Remove the "connector-" prefix in Maven module names

---
+++ aws-sns-sink/src/test/java/dev/knative/eventing/connector/AwsSnsSinkTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.knative.eventing.connector;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.ResourceArg;
+import io.quarkus.test.junit.QuarkusTest;
+import org.citrusframework.GherkinTestActionRunner;
+import org.citrusframework.annotations.CitrusResource;
+import org.citrusframework.context.TestContext;
+import org.citrusframework.exceptions.CitrusRuntimeException;
+import org.citrusframework.http.client.HttpClient;
+import org.citrusframework.http.endpoint.builder.HttpEndpoints;
+import org.citrusframework.quarkus.CitrusSupport;
+import org.citrusframework.spi.BindToRegistry;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+import org.springframework.http.HttpStatus;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+
+import static org.citrusframework.container.RepeatOnErrorUntilTrue.Builder.repeatOnError;
+import static org.citrusframework.http.actions.HttpActionBuilder.http;
+
+@QuarkusTest
+@CitrusSupport
+@QuarkusTestResource(value = LocalstackTestResource.class, restrictToAnnotatedClass = true, initArgs = {
+        @ResourceArg(name = "queueNameOrArn", value = "myqueue"),
+        @ResourceArg(name = "topicNameOrArn", value = "mytopic")
+})
+public class AwsSnsSinkTest {
+
+    @CitrusResource
+    private GherkinTestActionRunner tc;
+
+    private final String snsData = "Hello from AWS SNS!";
+    private final String sqsQueueName = "myqueue";
+
+    @LocalstackTestResource.Injected
+    public SqsClient sqsClient;
+
+    @BindToRegistry
+    public HttpClient knativeTrigger = HttpEndpoints.http()
+                .client()
+                .requestUrl("http://localhost:8081")
+                .build();
+
+    @Test
+    public void shouldConsumeEvents() {
+        tc.given(
+            http().client(knativeTrigger)
+                    .send()
+                    .post("/")
+                    .message()
+                    .body(snsData)
+                    .header("ce-id", "@matches([0-9A-Z]{15}-[0-9]{16})@")
+                    .header("ce-type", "dev.knative.eventing.aws.sns")
+                    .header("ce-source", "dev.knative.eventing.aws-sns-source")
+                    .header("ce-subject", "aws-sns-source")
+        );
+
+        tc.when(
+            http().client(knativeTrigger)
+                    .receive()
+                    .response(HttpStatus.NO_CONTENT)
+        );
+
+        tc.then(repeatOnError()
+                .autoSleep(1000L)
+                .until((i, context) -> i > 10)
+                .actions(this::verifySnsData));
+    }
+
+    private void verifySnsData(TestContext context) {
+        ListQueuesResponse listQueuesResult = sqsClient.listQueues(b ->
+                b.maxResults(100).queueNamePrefix(sqsQueueName));
+
+        String queueUrl = listQueuesResult.queueUrls().stream()
+                .filter(url -> url.endsWith("/" + sqsQueueName))
+                .findFirst()
+                .orElseThrow(() -> new CitrusRuntimeException("Queue %s not found".formatted(sqsQueueName)));
+
+        ReceiveMessageResponse receiveMessageResponse = sqsClient.receiveMessage(b ->
+                b.queueUrl(queueUrl));
+
+        try {
+            Assertions.assertEquals(1, receiveMessageResponse.messages().size());
+            Assertions.assertTrue(receiveMessageResponse.messages().get(0).body().contains(snsData));
+        } catch (AssertionFailedError error) {
+            throw new CitrusRuntimeException("SNS data verification failed", error);
+        }
+    }
+
+}
