6b799374da8d51f746e65cfb93cfc100c253b724
David Simansky <dsimansk@redhat.com>
2024-12-11 15:51:52 +0100

    Add initial support to produce images in ConfigMap as release artifact (#45)

--- hack/release.sh
+++ hack/release.sh
@@ -17,6 +17,9 @@
 # shellcheck disable=SC1090
 source "$(go run knative.dev/hack/cmd/script release.sh)"
 
+CONFIMAP_YAML="eventing-integrations-images.yaml"
+IMAGES_TXT="images.txt"
+
 function build_release() {
   header "Building images"
 
@@ -25,6 +28,38 @@ function build_release() {
   ./mvnw clean package -P knative -DskipTests || return $?
 
   echo "Image build & push completed"
+
+  echo "Collect image shas into ConfigMap"
+  generate_configMap
+
+  ARTIFACTS_TO_PUBLISH="${CONFIMAP_YAML} ${IMAGES_TXT}"
+  sha256sum ${ARTIFACTS_TO_PUBLISH} >checksums.txt
+  ARTIFACTS_TO_PUBLISH="${ARTIFACTS_TO_PUBLISH} checksums.txt"
+  echo "Checksum:"
+  cat checksums.txt
+}
+
+function generate_configMap() {
+  image_digests=$(find . -name "jib-image.digest")
+
+  cat <<EOF > ${CONFIMAP_YAML}
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: eventing-integrations-images
+    data:
+EOF
+
+  # shellcheck disable=SC2068
+  for image_digest_path in ${image_digests[@]}; do
+    echo "Processing image: ${image_digest_path}"
+    image=$(echo "${image_digest_path}" | cut -d "/" -f2)
+    image_digest=$(cat "${image_digest_path}")
+    # Config map with key value imageName:imageRef
+    echo "  ${image}: ${KO_DOCKER_REPO}/${image}:${image_digest}" >>${CONFIMAP_YAML}
+    # Storing plain image URLs in a txt file
+    echo "${KO_DOCKER_REPO}/${image}:${image_digest}" >>${IMAGES_TXT}
+  done
 }
 
 main "$@"
